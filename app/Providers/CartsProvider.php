8002e8283263692732d93f1d03c803e09ad10322
ngocthanh909 <thanhstp99@gmail.com>
2021-01-15 20:16:44 +0700

    fix provider

--- app/Providers/CartsProvider.php
+++ app/Providers/CartsProvider.php
@@ -25,24 +25,27 @@ class CartsProvider extends ServiceProvider
     {
         //
         view()->composer('user.layout.layout1', function ($view) {
-            if (session()->get('loginData')['logged'] == 1){
+            if(isset(session()->get('loginData')['logged'])){
+                if (session()->get('loginData')['logged'] == 1){
                 
-                session()->forget('cartsData');
-                $userID = session()->get('loginData')['data']['UserID'];
-                $cartDB = DB::table('cart')->where('UserID' , '=' , $userID)->get();
-                $cartsData[] = array ();
-                foreach ($cartDB as $cartItem){
-                    $product = DB::table('product')->where('ProductID' , '=' , $cartItem->ProductID)->first();
-                    $productPush = array (
-                        'ProductID' => $cartItem->ProductID,
-                        'Quantity' => $cartItem->Quality,
-                        'ProductName' => $product->Name,
-                        'ProductSKU' => $product->Sku,
-                        'ProductPrice' => $product->Price,
-                    );
-                    session()->push('cartsData', $productPush);
+                    session()->forget('cartsData');
+                    $userID = session()->get('loginData')['data']['UserID'];
+                    $cartDB = DB::table('cart')->where('UserID' , '=' , $userID)->get();
+                    $cartsData[] = array ();
+                    foreach ($cartDB as $cartItem){
+                        $product = DB::table('product')->where('ProductID' , '=' , $cartItem->ProductID)->first();
+                        $productPush = array (
+                            'ProductID' => $cartItem->ProductID,
+                            'Quantity' => $cartItem->Quality,
+                            'ProductName' => $product->Name,
+                            'ProductSKU' => $product->Sku,
+                            'ProductPrice' => $product->Price,
+                        );
+                        session()->push('cartsData', $productPush);
+                    }
                 }
             }
         });
     }
 }
+
